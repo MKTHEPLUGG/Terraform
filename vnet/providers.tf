ca8646ba086719b502aacc10473adaebb21e24ee
MetiMike <it.mvha@gmail.com>
2023-10-18 13:22:27 +0200

    creating workflow to use remote action for deploy

---
+++ vnet/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.76.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.3.7"
+}
+
+provider "azurerm" {
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  features {
+
+  }
+}
